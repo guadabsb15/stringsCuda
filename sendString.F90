58c0310533575f79a67c0eafea4889c63c04d0da
guadabsb15 <guadabsb15@gmail.com>
2017-06-07 12:44:55 -0400

    sending an integer from one subroutine to other on the device, change value of variable than can be then accessed by device

--- sendString.F90
+++ sendString.F90
@@ -2,7 +2,7 @@ module mathOps
 
 implicit none
 
-integer:: log_error
+integer, allocatable :: log_error
 attributes(managed) log_error
 
 contains
@@ -21,7 +21,7 @@ contains
    implicit none
    integer, intent(in) ::error
    
-   log_error = 3
+   log_error = error
  end subroutine log 
 end module mathOps
 
@@ -35,17 +35,20 @@ program testSaxpy
   type(dim3) :: grid, tBlock
   integer :: istat
 
+  allocate(log_error)
   tBlock = dim3(256,1,1)
   grid = dim3(ceiling(real(N)/tBlock%x),1,1)
 
   x = 1.0; y = 2.0; a = 2.0
   x_d = x
   y_d = y
-  !call saxpy<<<grid, tBlock>>>(x_d, y_d, a)
-  call saxpy<<<1,1>>>(x_d,y_d,a)
+  call saxpy<<<grid, tBlock>>>(x_d, y_d, a)
+  !call saxpy<<<1,1>>>(x_d,y_d,a)
   y = y_d
   write(*,*) 'Max error: ', maxval(abs(y-4.0))
 
   istat = cudaDeviceSynchronize()
-  write(*,*) 'my log thing', log_error  
+  if (log_error>0) write(*,*) 'called the error function', log_error
+
+deallocate(log_error)  
 end program testSaxpy
